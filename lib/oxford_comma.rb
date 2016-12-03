6893bd76ea0df39819e0f686bac2f19508bdb88f
MOHAMMAD DAYLAKI <mohammad.daylaky@gmail.com>
2016-12-03 13:52:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+  if array.size ==3
+     last_item =array.pop
+     array.push("and #{last_item}")
+     array.join(", ")
+   elsif array.size > 3
+     last_item2 =array.pop
+     array.push("and #{last_item2}")
+     array.join(", ")
+  else
+    array.join
+    array.join(" and ")
+  end
 end
